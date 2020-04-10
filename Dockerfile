601d270bb718f9e6927cffc32104dbea919217e3
Tigran Mkrtchyan <mail@kofemann.dev>
2020-04-10 13:09:04 +0200

    add certificate generation

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,16 @@
 FROM cfssl/cfssl:latest
 
+RUN mkdir -p /opt/acc/etc /opt/acc/ca /opt/acc/out
+
+
+COPY ca-csr.json /opt/acc/etc/ca-csr.json
+COPY intermediate-csr.json /opt/acc/etc/intermediate-csr.json
+COPY server-csr.json /opt/acc/etc/server-csr.json
+
+COPY signing-ca-cert-config.json /opt/acc/etc/signing-ca-cert-config.json
+
 COPY entrypoint.sh /entrypoint.sh
+
+
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,27 @@
 #!/bin/sh
+
+ACC_HOME=/opt/acc
+LOG=-loglevel=4
+
+# Generate root-ca. The generated file are root-ca.pem and root-ca.key
+cfssl genkey $LOG -initca ${ACC_HOME}/etc/ca-csr.json \
+| cfssljson -bare ${ACC_HOME}/ca/root-ca
+
+
+# Generate signing-ca. The generated file are signing-ca.pem, signing-ca.key and signing-ca.csr
+cfssl gencert $LOG -ca ${ACC_HOME}/ca/root-ca.pem -ca-key ${ACC_HOME}/ca/root-ca-key.pem \
+    -config="${ACC_HOME}/etc/signing-ca-cert-config.json" \
+    -profile="intermediate" ${ACC_HOME}/etc/intermediate-csr.json | \
+    cfssljson -bare ${ACC_HOME}/ca/signing-ca
+
+# Generate server sertificate.
+cfssl gencert $LOG -ca ${ACC_HOME}/ca/signing-ca.pem -ca-key ${ACC_HOME}/ca/signing-ca-key.pem \
+    -config="${ACC_HOME}/etc/signing-ca-cert-config.json" \
+    -profile="server" ${ACC_HOME}/etc/server-csr.json | \
+    cfssljson -bare ${ACC_HOME}/out/host
+
+
+# CA-cain
+cat ${ACC_HOME}/ca/root-ca.pem ${ACC_HOME}/ca/signing-ca.pem > ${ACC_HOME}/out/ca-chain.pem
+
+exec /bin/bash
